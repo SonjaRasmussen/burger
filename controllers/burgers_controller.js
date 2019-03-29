202d85b2ec21bf94d9b64b63b30e3350ec09c3df
Sonja Rasmussen <sonja.rasmussen@gmail.com>
2019-03-28 18:25:35 -0700

    fixed posting issues

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -27,7 +27,7 @@
         });
  });
 
- router.put("/burgers/updateOne:id", function(req, res){
+ router.post("/burgers/updateOne/:id", function(req, res){
      var condition = "id = " + req.params.id;
 
      console.log("condition", condition);
